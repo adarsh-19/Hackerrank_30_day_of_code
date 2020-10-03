4d7c7b73716c570abc9fb28b1b1d795989cf592e
Adarsh Tiwari <63408871+adarsh-19@users.noreply.github.com>
2020-10-03 14:40:10 +0530

    Create Day-0 Hello,World.cpp

---
+++ Day-0 Hello,World.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{ 
+    string s;
+    getline(cin,s);
+    cout<<"Hello, World.\n"<<s<<endl;
+    return 0;
+}
